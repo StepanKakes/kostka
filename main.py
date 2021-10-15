1c00383ac96f6f587802f0c26a533e3c1b36de63
Štěpán Kakeš <91247524+StepanKakes@users.noreply.github.com>
2021-10-15 17:17:35 +0200

    Aktualizovat main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,116 @@
+povoleno = False
+roll = 0
+pick=6
+def on_logo_event_pressed(): 
+    global pick
+    if pick == 10:
+        pick = 6
+        basic.show_number(6)
+    else:
+        pick = 10
+        basic.show_number(10)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
+
+def on_button_pressed_a():
+    global povoleno
+    povoleno=True
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 def on_forever():
-
-    def on_button_pressed_a():
-
-        def on_gesture_shake():  
-            basic.show_number(randint(1, 6))
-        input.on_gesture(Gesture.SHAKE, on_gesture_shake)
-    input.on_button_pressed(Button.A, on_button_pressed_a)
+    global povoleno
+    if povoleno:
+        if input.is_gesture(Gesture.SHAKE):
+            roll = randint(1, pick) 
+            
+            if roll == 1:
+                basic.show_leds("""
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+                """)
+                music.play_melody("C5 - - - - - - - ", 700)
+            if roll == 2:
+                basic.show_leds("""
+                    # . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . #
+                """)
+                music.play_melody("C5 - C5 - - - - - ", 700)
+            if roll == 3:
+                basic.show_leds("""
+                    # . . . .
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    . . . . #
+                """)
+                music.play_melody("C5 - C5 - C5 - - - ", 700)
+            if roll == 4:
+                basic.show_leds("""
+                    # . . . #
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    # . . . #
+                """)
+                music.play_melody("C5 - C5 - C5 - C5 - ", 700)
+            if roll == 5:
+                basic.show_leds("""
+                    # . . . #
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    # . . . #
+                """)
+                music.play_melody("C5 - C5 - C5 - C5 - C5 -", 700)
+            if roll == 6:
+                basic.show_leds("""
+                    # . . . #
+                    . . . . .
+                    # . . . #
+                    . . . . .
+                    # . . . #
+                """)
+                music.play_melody("C5 - C5 - C5 - C5 - C5 - C5", 700)
+            if roll == 7:
+                basic.show_leds("""
+                    # . . . #
+                    . . . . .
+                    # . # . #
+                    . . . . .
+                    # . . . #
+                """)
+                music.play_melody("C5 - C5 - C5 - C5 - C5 - C5 - C5", 700)
+            if roll == 8:
+                basic.show_leds("""
+                    # . . . #
+                    . . # . .
+                    # . . . #
+                    . . # . .
+                    # . . . #
+                """)
+                music.play_melody("C5 - C5 - C5 - C5 - C5 - C5 - C5 - C5", 700)
+            if roll == 9:
+                basic.show_leds("""
+                    # . # . #
+                    . . . . .
+                    # . # . #
+                    . . . . .
+                    # . # . #
+                """)
+                music.play_melody("C5 - C5 - C5 - C5 - C5 - C5 - C5 - C5 - C5", 700)
+            if roll == 10:
+                basic.show_leds("""
+                    # . . . #
+                    # . . . #
+                    # . . . #
+                    # . . . #
+                    # . . . #
+                """)
+                music.play_melody("C5 - C5 - C5 - C5 - C5 - C5 - C5 - C5 - C5 - C5", 700)
+            povoleno = False
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,7 +1,140 @@
+let povoleno = false
+let roll = 0
+let pick = 6
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    
+    if (pick == 10) {
+        pick = 6
+        basic.showNumber(6)
+    } else {
+        pick = 10
+        basic.showNumber(10)
+    }
+    
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    povoleno = true
+})
 basic.forever(function on_forever() {
-    input.onButtonPressed(Button.A, function on_button_pressed_a() {
-        input.onGesture(Gesture.Shake, function on_gesture_shake() {
-            basic.showNumber(randint(1, 6))
-        })
-    })
+    let roll: number;
+    
+    if (povoleno) {
+        if (input.isGesture(Gesture.Shake)) {
+            roll = randint(1, pick)
+            if (roll == 1) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+                `)
+                music.playMelody("C5 - - - - - - - ", 700)
+            }
+            
+            if (roll == 2) {
+                basic.showLeds(`
+                    # . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . #
+                `)
+                music.playMelody("C5 - C5 - - - - - ", 700)
+            }
+            
+            if (roll == 3) {
+                basic.showLeds(`
+                    # . . . .
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    . . . . #
+                `)
+                music.playMelody("C5 - C5 - C5 - - - ", 700)
+            }
+            
+            if (roll == 4) {
+                basic.showLeds(`
+                    # . . . #
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    # . . . #
+                `)
+                music.playMelody("C5 - C5 - C5 - C5 - ", 700)
+            }
+            
+            if (roll == 5) {
+                basic.showLeds(`
+                    # . . . #
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    # . . . #
+                `)
+                music.playMelody("C5 - C5 - C5 - C5 - C5 -", 700)
+            }
+            
+            if (roll == 6) {
+                basic.showLeds(`
+                    # . . . #
+                    . . . . .
+                    # . . . #
+                    . . . . .
+                    # . . . #
+                `)
+                music.playMelody("C5 - C5 - C5 - C5 - C5 - C5", 700)
+            }
+            
+            if (roll == 7) {
+                basic.showLeds(`
+                    # . . . #
+                    . . . . .
+                    # . # . #
+                    . . . . .
+                    # . . . #
+                `)
+                music.playMelody("C5 - C5 - C5 - C5 - C5 - C5 - C5", 700)
+            }
+            
+            if (roll == 8) {
+                basic.showLeds(`
+                    # . . . #
+                    . . # . .
+                    # . . . #
+                    . . # . .
+                    # . . . #
+                `)
+                music.playMelody("C5 - C5 - C5 - C5 - C5 - C5 - C5 - C5", 700)
+            }
+            
+            if (roll == 9) {
+                basic.showLeds(`
+                    # . # . #
+                    . . . . .
+                    # . # . #
+                    . . . . .
+                    # . # . #
+                `)
+                music.playMelody("C5 - C5 - C5 - C5 - C5 - C5 - C5 - C5 - C5", 700)
+            }
+            
+            if (roll == 10) {
+                basic.showLeds(`
+                    # . . . #
+                    # . . . #
+                    # . . . #
+                    # . . . #
+                    # . . . #
+                `)
+                music.playMelody("C5 - C5 - C5 - C5 - C5 - C5 - C5 - C5 - C5 - C5", 700)
+            }
+            
+            povoleno = false
+        }
+        
+    }
+    
 })
